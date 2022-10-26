697e6dfb590b1d4938bc7dc41ff38e488c361a16
NorthMarshmallo <varyaelshina@mail.ru>
2022-10-26 19:52:05 +0300

    Запушено 3 задание

--- src/ru/croc/task3/Main.java
+++ src/ru/croc/task3/Main.java
@@ -1,2 +1,34 @@
-package ru.croc.task3;public class Main {
+package ru.croc.task3;
+import java.util.Scanner;
+
+public class Main {
+    static class Point {
+        double x, y;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        Point a = new Point();
+        a.x = in.nextDouble();
+        a.y = in.nextDouble();
+
+        Point b = new Point();
+        b.x = in.nextDouble();
+        b.y = in.nextDouble();
+
+        Point c = new Point();
+        c.x = in.nextDouble();
+        c.y = in.nextDouble();
+
+        // использовано векторное произведение векторов на ab и ac, на которых строится треугольник
+        // третья координата взята равной нулю для каждого вектора
+        // итого в произведении остается только одно слагаемое с определителем
+        double det = (b.x - a.x) * (c.y - a.y) - (c.x - a.x) * (b.y - a.y);
+        double area = 0.5 * Math.sqrt( Math.pow(det, 2) );
+
+        System.out.println("Area of the triangle: " + area);
+        in.close();
+    }
 }
